2f54a0dd5f29e3fa4059abdac6c5fa05dcb40aa5
msimms <msimms@salesforce.com>
2014-06-11 13:06:06 -0300

    updated emacs to refresh on file change and added backspace

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,3 +75,7 @@
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; Matt custom
+(global-auto-revert-mode t)
+(normal-erase-is-backspace-mode 0)
